2be36600df01a876fbacf7c2d2bfad4a631008ca
reader00 <qoryamanahputra0708@gmail.com>
2022-02-11 02:28:12 +0700

    add 'reply' to thread helper

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -9,8 +9,8 @@ const ThreadsTableTestHelper = {
         owner = 'user-123',
     }) {
         const query = {
-            text: 'INSERT INTO threads VALUES($1, $2, $3, $4) RETURNING id',
-            values: [id, title, body, owner],
+            text: 'INSERT INTO threads(id, title, body, owner, date) VALUES($1, $2, $3, $4, $5) RETURNING id',
+            values: [id, title, body, owner, new Date().toISOString()],
         };
 
         const results = await pool.query(query);
@@ -24,8 +24,23 @@ const ThreadsTableTestHelper = {
         owner = 'user-123',
     }) {
         const query = {
-            text: 'INSERT INTO comments(id, thread_id, content, owner) VALUES($1, $2, $3, $4) RETURNING id',
-            values: [id, threadId, content, owner],
+            text: 'INSERT INTO comments(id, thread_id, content, owner, date) VALUES($1, $2, $3, $4, $5) RETURNING id',
+            values: [id, threadId, content, owner, new Date().toISOString()],
+        };
+
+        const results = await pool.query(query);
+        return results.rows[0].id;
+    },
+
+    async addReply({
+        id = 'reply-123',
+        commentId = 'comment-123',
+        content = 'Hai, apa kabar',
+        owner = 'user-123',
+    }) {
+        const query = {
+            text: 'INSERT INTO replies(id, comment_id, content, owner, date) VALUES($1, $2, $3, $4, $5) RETURNING id',
+            values: [id, commentId, content, owner, new Date().toISOString()],
         };
 
         const results = await pool.query(query);
@@ -35,6 +50,15 @@ const ThreadsTableTestHelper = {
     async deleteComment({ id = 'comment-123', owner = 'user-123' }) {
         const query = {
             text: `UPDATE comments SET is_deleted = 1 WHERE id = $1 AND owner = $2 RETURNING id`,
+            values: [id, owner],
+        };
+
+        await pool.query(query);
+    },
+
+    async deleteReply({ id = 'reply-123', owner = 'user-123' }) {
+        const query = {
+            text: `UPDATE replies SET is_deleted = 1 WHERE id = $1 AND owner = $2 RETURNING id`,
             values: [id, owner],
         };
 
@@ -61,9 +85,20 @@ const ThreadsTableTestHelper = {
         return result.rows;
     },
 
+    async findReplyById(id) {
+        const query = {
+            text: 'SELECT * FROM replies WHERE id = $1',
+            values: [id],
+        };
+
+        const result = await pool.query(query);
+        return result.rows;
+    },
+
     async cleanTable() {
         await pool.query('DELETE FROM threads');
         await pool.query('DELETE FROM comments');
+        await pool.query('DELETE FROM replies');
     },
 };
 
