eac2abd903e341ea054f90aa0be9bf87c6a34967
reader00 <qoryamanahputra0708@gmail.com>
2022-02-10 22:37:44 +0700

    add 'Delete comment' entity

---
+++ src/Domains/threads/entities/_test/DeleteComment.test.js
@@ -0,0 +1,44 @@
+const DeleteComment = require('../DeleteComment');
+
+describe('a DeleteComment entities', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        // Arrange
+        const payload = {
+            threadId: 123,
+        };
+
+        // Action and Assert
+        expect(() => new DeleteComment(payload)).toThrowError('DELETE_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload did not meet data type specification', () => {
+        // Arrange
+        const payload = {
+            threadId: 123,
+            commentId: [],
+            owner: 'user-123',
+        };
+
+        // Action and Assert
+        expect(() => new DeleteComment(payload)).toThrowError(
+            'DELETE_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+        );
+    });
+
+    it('should create DeleteComment object correctly', () => {
+        // Arrange
+        const payload = {
+            threadId: 'thread-123',
+            commentId: 'comment-123',
+            owner: 'user-123',
+        };
+
+        // Action
+        const { threadId, commentId, owner } = new DeleteComment(payload);
+
+        // Assert
+        expect(threadId).toEqual(payload.threadId);
+        expect(commentId).toEqual(payload.commentId);
+        expect(owner).toEqual(payload.owner);
+    });
+});
