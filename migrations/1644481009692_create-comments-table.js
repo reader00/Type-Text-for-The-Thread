a88198728b8045138eade79db477ad397d035668
reader00 <qoryamanahputra0708@gmail.com>
2022-02-10 16:38:29 +0700

    create comments table migration

---
+++ migrations/1644481009692_create-comments-table.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+    pgm.createTable('comments', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        thread_id: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        content: {
+            type: 'VARCHAR(512)',
+            notNull: true,
+        },
+        owner: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+        },
+        date: {
+            type: 'TIMESTAMP',
+            default: 'NOW()',
+        },
+    });
+
+    pgm.addConstraint(
+        'comments',
+        'fk_comments.thread_id_threads.id',
+        'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE'
+    );
+    pgm.addConstraint(
+        'comments',
+        'fk_comments.user_id_users.id',
+        'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE'
+    );
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('comments');
+};
