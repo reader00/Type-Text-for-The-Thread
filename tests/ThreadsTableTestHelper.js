5776c73f067bb88b0a15a9ee113d1e45235834db
reader00 <qoryamanahputra0708@gmail.com>
2022-02-10 16:40:45 +0700

    add additional method and fixing Thread helper

--- tests/ThreadsTableTestHelper.js
+++ tests/ThreadsTableTestHelper.js
@@ -2,18 +2,34 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadsTableTestHelper = {
-    async addThread({ id = 'thread-123', title = 'Di atas Awan', body = 'Ku ingin terbang' }) {
+    async addThread({
+        id = 'thread-123',
+        title = 'Di atas Awan',
+        body = 'Ku ingin terbang',
+        owner = 'user-123',
+    }) {
         const query = {
-            text: 'INSERT INTO threads VALUES($1, $2, $3)',
-            values: [id, title, body],
+            text: 'INSERT INTO threads VALUES($1, $2, $3, $4) RETURNING id',
+            values: [id, title, body, owner],
         };
 
-        await pool.query(query);
+        const results = await pool.query(query);
+        return results.rows[0].id;
     },
 
     async findThreadById(id) {
         const query = {
             text: 'SELECT * FROM threads WHERE id = $1',
+            values: [id],
+        };
+
+        const result = await pool.query(query);
+        return result.rows;
+    },
+
+    async findCommentById(id) {
+        const query = {
+            text: 'SELECT * FROM comments WHERE id = $1',
             values: [id],
         };
 
