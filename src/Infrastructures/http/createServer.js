6b59b8bc9dba6700cd5dfdcc8df7ae238e838a32
reader00 <qoryamanahputra0708@gmail.com>
2022-02-10 12:00:38 +0700

    add JWT

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,6 +1,9 @@
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
 const plugins = require('../../Interfaces/http/api');
 const extensions = require('../../Interfaces/http/extensions');
+const { jwtStrategy } = require('./strategy');
 
 const createServer = async (container) => {
     const server = Hapi.server({
@@ -8,8 +11,19 @@ const createServer = async (container) => {
         port: process.env.PORT,
     });
 
+    // Register JWT plugin
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    // JWT strategy
+    server.auth.strategy('forumapi_jwt', 'jwt', jwtStrategy);
+
     await server.register(plugins(container));
 
+    // Set onPreResponse for error handler
     server.ext('onPreResponse', extensions.onPreResponse);
 
     return server;
