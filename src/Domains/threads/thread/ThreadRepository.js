31a79bd8d6311a42ffd1621b46c5ef1f6efff539
reader00 <qoryamanahputra0708@gmail.com>
2022-02-13 16:21:44 +0700

    delete comment and replies function

--- src/Domains/threads/thread/ThreadRepository.js
+++ src/Domains/threads/thread/ThreadRepository.js
@@ -1,45 +1,13 @@
 class ThreadRepository {
-    async addThread(addThread) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
     async verifyThreadExist(threadId) {
         throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 
-    async verifyCommentExist(data) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async verifyReplyExist(data) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async addComment(addComment) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async addReply(addReply) {
+    async addThread(addThread) {
         throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 
     async getThreadDetailsById(threadId) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async getThreadCommentsById(threadId) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async getCommentRepliesById(threadId) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async deleteCommentById(commentId) {
-        throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-    }
-
-    async deleteReplyById(replyId) {
         throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     }
 }
--- src/Domains/threads/thread/_test/ThreadRepository.test.js
+++ src/Domains/threads/thread/_test/ThreadRepository.test.js
@@ -6,37 +6,13 @@ describe('ThreadRepository interface', () => {
         const threadRepository = new ThreadRepository();
 
         // Action and Assert
-        await expect(threadRepository.addThread({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
         await expect(threadRepository.verifyThreadExist()).rejects.toThrowError(
             'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
         );
-        await expect(threadRepository.verifyCommentExist()).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.verifyReplyExist()).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.addComment({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.addReply({})).rejects.toThrowError(
+        await expect(threadRepository.addThread({})).rejects.toThrowError(
             'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
         );
         await expect(threadRepository.getThreadDetailsById({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.getThreadCommentsById({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.getCommentRepliesById({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.deleteCommentById({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
-        );
-        await expect(threadRepository.deleteReplyById({})).rejects.toThrowError(
             'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
         );
     });
