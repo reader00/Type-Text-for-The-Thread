9a2eb5d10c6d4f56527cdd268df27b45d1ce6fd8
reader00 <qoryamanahputra0708@gmail.com>
2022-02-17 15:58:43 +0700

    add like repo to get thread use case

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -253,6 +253,10 @@ container.register([
                     name: 'replyRepository',
                     internal: ReplyRepository.name,
                 },
+                {
+                    name: 'likeRepository',
+                    internal: LikeRepository.name,
+                },
             ],
         },
     },
