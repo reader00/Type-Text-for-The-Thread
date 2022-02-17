14fe0ac5456077dd7602352fa3a02ea232f1ffa2
reader00 <qoryamanahputra0708@gmail.com>
2022-02-17 17:07:38 +0700

    chage thre repo method singular to plural

--- src/Domains/threads/thread/_test/ThreadRepository.test.js
+++ src/Domains/threads/thread/_test/ThreadRepository.test.js
@@ -12,8 +12,8 @@ describe('ThreadRepository interface', () => {
         await expect(threadRepository.addThread({})).rejects.toThrowError(
             'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
         );
-        await expect(threadRepository.getThreadDetailsById({})).rejects.toThrowError(
-            'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
-        );
+        await expect(
+            threadRepository.getThreadDetailById({}),
+        ).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     });
 });
